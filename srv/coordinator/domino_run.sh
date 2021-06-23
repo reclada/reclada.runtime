332d47b0ab84a45b30596c8393fdb37f699e4766
andreylr <andrey.rumyantsev@quantori.com>
2021-06-23 11:06:23 +0300

    The name of environment variables were changed.

--- srv/coordinator/domino_run.sh
+++ srv/coordinator/domino_run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 
-python3 coordinator.py -paltform DOMINO -database POSTGRESQL -messenger POSTGRESQL
+export POSTGRES_HOST="34.252.72.157"
+export POSTGRES_DB="reclada_dev_1"
+export POSTGRES_USER="rumyantsev"
+export POSTGRES_PASSWORD="LjgPsdert8we8"
+export POSTGRES_NOTIFY_CHANNEL="test"
+
+python3 -m srv.coordinator.coordinator -platform DOMINO -database POSTGRESQL -messenger POSTGRESQL
