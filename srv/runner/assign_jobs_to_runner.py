7289f11fd8a39b589d001086668feaca92541cd2
mhemt <74146735+mhemt@users.noreply.github.com>
2021-06-29 17:34:43 +0300

    Manual assignment of runner to jobs.

---
+++ srv/runner/assign_jobs_to_runner.py
@@ -0,0 +1,61 @@
+import argparse
+import json
+import os
+from urllib.parse import urlparse
+
+import psycopg2
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type')
+    parser.add_argument('--runner-id')
+    return parser
+
+
+def main():
+    parser = init_argparse()
+    args = parser.parse_args()
+
+    credentials = urlparse(os.getenv('DB_URI'))
+
+    connection = psycopg2.connect(
+        host=credentials.hostname,
+        database=credentials.path[1:],
+        user=credentials.username,
+        password=credentials.password,
+    )
+
+    cursor = connection.cursor()
+    data = {
+        'class': 'Job',
+        'attrs': {
+            'type': args.type,
+            'status': 'new',
+        },
+    }
+
+    cursor.callproc('reclada_object.list', [json.dumps(data)])
+    jobs = cursor.fetchall()[0][0]
+
+    for job in jobs:
+        data = {
+            'class': 'Job',
+            'id': job['id'],
+            'attrs': {
+                'type': job['attrs']['type'],
+                'task': job['attrs']['task'],
+                'command': job['attrs']['command'],
+                'input_parameters': job['attrs']['inputParameters'],
+                'status': 'pending',
+                'runner': args.runner_id,
+            },
+        }
+        cursor.callproc('reclada_object.update', [json.dumps(data)])
+
+    connection.commit()
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
