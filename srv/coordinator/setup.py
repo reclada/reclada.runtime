e3df5c619fac0ab215462abb92f8ddfa25bf78ce
andreylr <andrey.rumyantsev@quantori.com>
2021-07-01 12:11:25 +0300

    Added setup information

--- srv/coordinator/setup.py
+++ srv/coordinator/setup.py
@@ -2,4 +2,13 @@ from setuptools import setup
 
 exec(open("_version.py").read())
 
+setup(name='Distutils',
+      version=__version__,
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
+
 setup(version=__version__)
