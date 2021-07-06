869edd5f9bf2ead6127687bd52301c370a180543
Alexey Dobrov <alexey.dobrov@quantori.com>
2021-07-06 18:15:21 +0300

    added file id parameter

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 _S3_FILE_URI="$1"
-_JOB_ID="$2"
+_FILE_ID="$2"
+_JOB_ID="$3"
 _INPUT_DIR="/mnt/input/${_JOB_ID}"
 _OUTPUT_DIR="/mnt/output/${_JOB_ID}"
 
@@ -15,7 +16,7 @@ aws s3 cp "${_S3_FILE_URI}" "${_INPUT_DIR}/${S3_FILE_NAME}"
 
 python3 -m table_extractor.run run "${_INPUT_DIR}/${S3_FILE_NAME}" "${_OUTPUT_DIR}" --verbose true --paddle_on true
 
-python3 -m bd2reclada "${_OUTPUT_DIR}/${S3_FILE_NAME}/document.json" "${_OUTPUT_DIR}/output.csv"
+python3 -m bd2reclada "${_OUTPUT_DIR}/${S3_FILE_NAME}/document.json" "${_OUTPUT_DIR}/output.csv" "${_FILE_ID}"
 cat "${_OUTPUT_DIR}/output.csv" | psql ${DB_URI_QUOTED} -c "\COPY reclada.staging FROM STDIN WITH CSV QUOTE ''''"
 
 python3 -m lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
