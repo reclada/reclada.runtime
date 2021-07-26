dcd8da76b3fe1c8a1962c7e9f7ede0d43fff278e
AndreyRuma <andrey.rumyantsev@quantori.com>
2021-07-26 18:05:34 +0300

    Adding logging messages to run_pipeline.sh script.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -10,22 +10,44 @@ export _OUTPUT_DIR="/mnt/output/${_JOB_ID}"
 export PYTHONPATH="${PYTHONPATH}:${BADGERDOC_REPO_PATH}"
 export PYTHONPATH="${PYTHONPATH}:${SCINLP_REPO_PATH}"
 
+printf "STEP 1 - Begin - Parsing DB_URI environment variable\n"
 DB_URI_QUOTED=`python3 -c "import urllib.parse; parsed = urllib.parse.urlparse('$DB_URI'); print('$DB_URI'.replace(parsed.password, urllib.parse.quote(parsed.password)))"`
 S3_FILE_NAME=`python3 -c "print('$_S3_FILE_URI'.split('/')[-1])"`
+printf "STEP 1 - End\n"
 
+printf "STEP 2 - Begin - Copying files from S3 bucket to local drive\n"
 aws s3 cp "${_S3_FILE_URI}" "${_INPUT_DIR}/${S3_FILE_NAME}"
+printf "STEP 2 - End \n"
 
+printf "STEP 3 - Begin - Running badgerdoc \n"
 python3 -m table_extractor.run run "${_INPUT_DIR}/${S3_FILE_NAME}" "${_OUTPUT_DIR}" --verbose true --paddle_on true
+printf "STEP 3 - End\n"
 
-python3 -m bd2reclada "${_OUTPUT_DIR}/${S3_FILE_NAME}/document.json" "${_OUTPUT_DIR}/output.csv" "${_FILE_ID}"
-cat "${_OUTPUT_DIR}/output.csv" | psql ${DB_URI_QUOTED} -c "\COPY reclada.staging FROM STDIN WITH CSV QUOTE ''''"
-
-python3 -m lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
-cat "${_OUTPUT_DIR}/nlp_output.csv" | psql ${DB_URI_QUOTED} -c "\COPY reclada.staging FROM STDIN WITH CSV QUOTE ''''"
-
+printf "STEP 4 - Begin - Copying the results of badgerdoc's work to the S3 bucket\n"
 aws s3 cp "${_OUTPUT_DIR}" "s3://${AWS_S3_BUCKET_NAME}/output/${_JOB_ID}" --recursive
+printf "STEP 4 - End"
+
+printf "STEP 5 - Begin - Starting bd2reclada\n"
+python3 -m bd2reclada "${_OUTPUT_DIR}/${S3_FILE_NAME}/document.json" "${_OUTPUT_DIR}/output.csv" "${_FILE_ID}"
+printf "STEP 5 - End\n"
+
+printf "STEP 6 - Begin - Loading data to DB\n"
+cat "${_OUTPUT_DIR}/output.csv" | psql ${DB_URI_QUOTED} -c "\COPY reclada.staging FROM STDIN WITH CSV QUOTE ''''"
+printf "STEP 6 - End\n"
+
+printf "STEP 7 - Begin - SciNLP processing\n"
+python3 -m lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
+printf "STEP 7 - End\n"
+
+printf "STEP 8 - Begin - Loading data to DB\n"
+cat "${_OUTPUT_DIR}/nlp_output.csv" | psql ${DB_URI_QUOTED} -c "\COPY reclada.staging FROM STDIN WITH CSV QUOTE ''''"
+printf "STEP 8 - End\n"
+
+printf "STEP 9 - Begin - Copying result files to the S3 bucket\n"
+aws s3 cp "${_OUTPUT_DIR}" "s3://${AWS_S3_BUCKET_NAME}/output/${_JOB_ID}" --recursive
+printf "STEP 9 - End\n"
 
 # On some platform such as DOMINO we need to add an extra step in the pipeline.
 # The extra step supplied as 4th parameter and if it exists then
 # we need to run it. If it doesn't then we skip this step
-[ ! -z "$_CUSTOM_TASK" ] && source "$_CUSTOM_TASK"
+[ ! -z "$_CUSTOM_TASK" ] && printf "STEP 10 - Begin\n" && source "$_CUSTOM_TASK" && printf "STEP 10 - End\n"
