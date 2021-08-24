9ee8da8a4eaa336637ba5b4aabe39757b906ea5f
mhemt <74146735+mhemt@users.noreply.github.com>
2021-08-24 18:49:13 +0300

    Add presigned url generation for post

--- aws_lambda/s3_get_presigned_url.py
+++ aws_lambda/s3_get_presigned_url.py
@@ -2,6 +2,8 @@
 Get presigned url for s3 object
 """
 import logging
+import os
+from datetime import datetime
 from urllib.parse import urlparse
 
 import boto3
@@ -13,9 +15,7 @@ logger.setLevel(logging.INFO)
 s3_client = boto3.client('s3')
 
 
-def lambda_handler(event, context):
-    logger.info(f'Event: {event}')
-
+def generate_presigned_get(event):
     parsed_uri = urlparse(event['uri'])
     bucket = parsed_uri.netloc
     key = parsed_uri.path.lstrip('/')
@@ -31,7 +31,67 @@ def lambda_handler(event, context):
         )
     except ClientError as err:
         logging.error(err)
-        return {}
+        return {'error': err}
 
-    # The response contains the presigned URL
+    # The response contains the presigned URL for downloading from S3
     return {'url': url}
+
+
+def generate_presigned_post(event):
+    key = os.path.join(
+        'inbox/',
+        datetime.strftime(datetime.now(), '%Y/%m/%d/%H-%M-%S-%f/'),
+        event['fileName'],
+    )
+
+    try:
+        response = s3_client.generate_presigned_post(
+            Bucket=event['bucketName'],
+            Key=key,
+            Fields={
+                'Content-Type': event['fileType'],
+            },
+            Conditions=[
+                {'Content-Type': event['fileType']},
+                ['content-length-range', 1, event['fileSize']],
+            ],
+            ExpiresIn=3600,
+        )
+    except ClientError as err:
+        logging.error(err)
+        return {'error': err}
+
+    # The response contains the presigned URL for uploading to S3
+    return response
+
+
+def lambda_handler(event, context):
+    logger.info(f'Event: {event}')
+
+    request_type = event['type']
+
+    if request_type == 'get':
+        return generate_presigned_get(event)
+    elif request_type == 'post':
+        return generate_presigned_post(event)
+    else:
+        return {'error': 'request type not supported'}
+
+
+if __name__ == '__main__':
+    event_get = {
+        'type': 'get',
+        'uri': 's3://bucket/key.pdf',
+        'expiration': 3600,
+    }
+    print(lambda_handler(event_get, None))
+
+    event_post = {
+        'type': 'post',
+        'bucketName': 'bucket',
+        'fileName': 'key.pdf',
+        'fileType': 'application/pdf',
+        'fileSize': 100,
+        'expiration': 3600,
+    }
+    print(lambda_handler(event_post, None))
