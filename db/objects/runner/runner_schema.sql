3ea6b559dfe1b9f190fd205db8bc7551457628ac
AlexandraVin <alex.vingert@gmail.com>
2021-09-14 17:14:21 +0300

    K8S->DOMINO

--- db/objects/runner/runner_schema.sql
+++ db/objects/runner/runner_schema.sql
@@ -26,7 +26,7 @@ SELECT reclada_object.create('
    "class": "Runner",
     "attributes": {
         "task": "512a3dde-23c7-4771-b180-20f8781ac084",
-        "type": "K8S",
+        "type": "DOMINO",
         "status": "down",
         "command": "",
         "environment": "7b196912-d973-40a9-b0e2-15ecbd921b2f"
