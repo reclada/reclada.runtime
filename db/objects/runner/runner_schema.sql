0198bbf09b8e61ff5c1b06e5e354790e1c12a328
AlexandraVin <81476464+AlexandraVin@users.noreply.github.com>
2021-09-22 16:56:10 +0300

    Update runner_schema.sql

--- db/objects/runner/runner_schema.sql
+++ db/objects/runner/runner_schema.sql
@@ -19,16 +19,3 @@ SELECT reclada_object.create_subclass('{
         "required": ["environment", "status"]
     }
 }'::jsonb);
-
-/* Just for demo */
-SELECT reclada_object.create('
-   {
-   "class": "Runner",
-    "attributes": {
-        "task": "512a3dde-23c7-4771-b180-20f8781ac084",
-        "type": "K8S",
-        "status": "down",
-        "command": "",
-        "environment": "7b196912-d973-40a9-b0e2-15ecbd921b2f"
-        }
-     }'::jsonb);
