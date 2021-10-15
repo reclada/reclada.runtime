4c13b3eadcd334b356d492a260fbb5e4c8a12e27
AndreyRuma <andrey.rumyantsev@quantori.com>
2021-10-15 14:59:39 +0300

    Updated run_pipeline.sh script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -55,27 +55,23 @@ error_check "ERROR happened during loading data to DB\n"
 printf "STEP 6 - End\n"
 
 printf "STEP 7 - Begin - SciNLP processing\n"
-python3 -m lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
+printf "This step is not implemented yet.\n"
+#python3 -m lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
 error_check "ERROR happened during SciNLP processing\n"
 printf "STEP 7 - End\n"
-
-printf "STEP 8 - Begin - Loading data to DB\n"
-cat "${_OUTPUT_DIR}/nlp_output.csv" | psql ${DB_URI_QUOTED} -c "\COPY reclada.staging FROM STDIN WITH CSV QUOTE ''''"
-error_check "ERROR happened during loading data to DB\n"
-printf "STEP 8 - End\n"
-
-printf "STEP 9 - Begin - Copying result files to the S3 bucket\n"
-aws s3 cp "${_OUTPUT_DIR}" "s3://${AWS_S3_BUCKET_NAME}/output/${_S3_OUTPUT_DIR}" --recursive --sse
-error_check "ERROR happened during copying results to the S3 bucket\n"
-printf "STEP 9 - End\n"
 
 # On some platform such as DOMINO we need to add an extra step in the pipeline.
 # The extra step supplied as 5th parameter and if it exists then
 # we need to run it. If it doesn't then we skip this step
 if [ -n "$_CUSTOM_TASK" ]
 then
-  printf "STEP 10 - Begin - Custom task\n"
+  printf "STEP 8 - Begin - Custom task\n"
   source "$_CUSTOM_TASK"
   error_check "ERROR happened during processing custom task\n"
-  printf "STEP 10 - End\n"
+  printf "STEP 8 - End\n"
 fi
+
+printf "STEP 9 - Begin - Copying result files to the S3 bucket\n"
+aws s3 cp "${_OUTPUT_DIR}" "s3://${AWS_S3_BUCKET_NAME}/output/${_S3_OUTPUT_DIR}" --recursive --sse
+error_check "ERROR happened during copying results to the S3 bucket\n"
+printf "STEP 9 - End\n"
