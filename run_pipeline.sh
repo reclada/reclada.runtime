ee5eba269e54100efc8c039db96df7540129eaa4
AndreyRuma <andrey.rumyantsev@quantori.com>
2021-12-02 16:24:55 +0300

    Run lite as a python module not as a package.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -71,8 +71,9 @@ error_check "ERROR happened during loading data to DB\n"
 printf "STEP 7 - End\n"
 
 printf "STEP 8 - Begin - SciNLP processing\n"
-echo $PYTHONPATH
-python3 -m lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
+cd "${SCINLP_REPO_PATH}/lite" || printf "Folder %s doesn't exist.\n" "${SCINLP_REPO_PATH}/lite"
+python3 lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
+cd - > /dev/null || printf "Can't return to the previous folder.\n"
 error_check "ERROR happened during SciNLP processing\n" "copy"
 printf "STEP 8 - End\n"
 
