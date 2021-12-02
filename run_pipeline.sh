a69bf5e6ae930e00799a729300484cf35b5b917a
AndreyRuma <andrey.rumyantsev@quantori.com>
2021-12-02 17:48:20 +0300

    Bug fix

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -72,7 +72,7 @@ printf "STEP 7 - End\n"
 
 printf "STEP 8 - Begin - SciNLP processing\n"
 cd "${SCINLP_REPO_PATH}" || printf "Folder %s doesn't exist.\n" "${SCINLP_REPO_PATH}"
-python3 lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
+python3 lite.py "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv"
 cd - > /dev/null || printf "Can't return to the previous folder.\n"
 error_check "ERROR happened during SciNLP processing\n" "copy"
 printf "STEP 8 - End\n"
