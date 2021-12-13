14000cd675b5f6e08066cf55965ec86eecb04a9b
AndreyRuma <andrey.rumyantsev@quantori.com>
2021-12-13 14:31:02 +0300

    Added one more step to pipe_line.sh. Step 9 coping data to DB

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -75,28 +75,33 @@ python3 -m lite "${_OUTPUT_DIR}/output.csv" "${_OUTPUT_DIR}/nlp_output.csv" "${D
 error_check "ERROR happened during SciNLP processing\n" "copy"
 printf "STEP 8 - End\n"
 
+printf "STEP 9 - Begin - Loading data to DB\n"
+cat "${_OUTPUT_DIR}/nlp_output.csv" | psql ${DB_URI_QUOTED} -c "\COPY reclada.staging FROM STDIN WITH CSV QUOTE ''''"
+error_check "ERROR happened during loading data to DB\n"
+printf "STEP 9 - End\n"
+
 # On some platform such as DOMINO we need to add an extra step in the pipeline.
 # The extra step supplied as 5th parameter and if it exists then
 # we need to run it. If it doesn't then we skip this step
 if [ -n "$_CUSTOM_TASK" ]
 then
-  printf "STEP 9 - Begin - Custom task\n"
+  printf "STEP 10 - Begin - Custom task\n"
   source "$_CUSTOM_TASK"
   error_check "ERROR happened during processing custom task\n" "copy"
-  printf "STEP 9 - End\n"
+  printf "STEP 10 - End\n"
 fi
 
 if [[ -n $_POSTPROCESS_TASK && $_POSTPROCESS_TASK != "0" ]]
 then
-  printf "STEP 10 - Begin - Running postprocess custom command\n"
+  printf "STEP 11 - Begin - Running postprocess custom command\n"
   source "$_POSTPROCESS_TASK"
   error_check "ERROR happened during postprocessing custom command\n" "copy"
-  printf "STEP 10 - End\n"
+  printf "STEP 11 - End\n"
 else
   printf "Postprocessing command is not set. Step 11 is skipped\n"
 fi
 
-printf "STEP 11 - Begin - Copying result files to the S3 bucket\n"
+printf "STEP 12 - Begin - Copying result files to the S3 bucket\n"
 aws s3 cp "${_OUTPUT_DIR}" "s3://${AWS_S3_BUCKET_NAME}/output/${_S3_OUTPUT_DIR}" --recursive --sse
 error_check "ERROR happened during copying results to the S3 bucket\n"
-printf "STEP 11 - End\n"
+printf "STEP 12 - End\n"
