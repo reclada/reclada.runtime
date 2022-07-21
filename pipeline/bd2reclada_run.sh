730b80d20bf89870253545bbd4d07443a6f44e00
sukharevsky <sukharevsky@yahoo.com>
2022-07-21 18:48:32 -0400

    Update bd2reclada_run.sh
    changed path

--- pipeline/bd2reclada_run.sh
+++ pipeline/bd2reclada_run.sh
@@ -25,7 +25,7 @@ S3_FILE_NAME=`python3 -c "print('$_S3_FILE_URI'.split('/')[-1])"`
 printf "STEP 2 - End\n"
 
 printf "STEP 3 - Begin - Starting bd2reclada\n"
-cd ${SCINLP_REPO_PATH}/src/srv
+cd ${SCINLP_REPO_PATH}
 python3 -m bd2reclada.src.__main__ "${_OUTPUT_DIR}/${S3_FILE_NAME}/document.json" "${_OUTPUT_DIR}/output.csv" "${_FILE_ID}"
 error_check "ERROR happened during running bd2reclada\n"
 printf "STEP 3 - End\n"
