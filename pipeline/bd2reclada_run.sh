e64bc65eb2dd52ddf302ef546503868fdd7711aa
sukharevsky <sukharevsky@yahoo.com>
2022-07-21 15:20:53 -0400

    Update bd2reclada_run.sh
    change to CURRENT_DIR

--- pipeline/bd2reclada_run.sh
+++ pipeline/bd2reclada_run.sh
@@ -13,6 +13,7 @@ export PYTHONPATH="${PYTHONPATH}:${SCINLP_REPO_PATH}/lite:${SCINLP_REPO_PATH}"
 printf "SCRIPT - Beging - Running bd2reclada utility for pipeline %s\n" "${_JOB_ID}"
 
 printf "STEP 1 - Begin - Restoring the pipeline context\n"
+CURRENT_DIR="${PWD}"
 source ./pipeline/get_context.sh
 error_check 'ERROR happened during restoring the pipeline context\n'
 printf "STEP 1 - End\n"
@@ -22,14 +23,14 @@ S3_FILE_NAME=`python3 -c "print('$_S3_FILE_URI'.split('/')[-1])"`
 printf "STEP 2 - End\n"
 
 printf "STEP 3 - Begin - Starting bd2reclada\n"
-cd /repos/SciNLP/src/srv/bd2reclada
+cd ${SCINLP_REPO_PATH}/src/srv/bd2reclada
 sudo python3 setup.py install
 python3 -m bd2reclada "${_OUTPUT_DIR}/${S3_FILE_NAME}/document.json" "${_OUTPUT_DIR}/output.csv" "${_FILE_ID}"
 error_check "ERROR happened during running bd2reclada\n"
 printf "STEP 3 - End\n"
 
 printf "STEP 4 - Begin - Saving the pipeline context\n"
-cd /repos/reclada.runtime
+cd ${CURRENT_DIR}
 source ./pipeline/save_context.sh 4
 error_check 'ERROR happened during saving the pipeline context\n'
 printf "STEP 4 - End\n"
